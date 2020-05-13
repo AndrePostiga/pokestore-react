b28141ec8a331d15046f25d3ec22c70d42488158
Andre Postiga <andre.postiga@msn.com>
2020-05-13 19:09:50 -0300

    First Commit and enviroment configuration

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Home() {
+  return (
+    <Container>
+      <h1>Hello Home</h1>
+      <Container />
+    </Container>
+  );
+}
