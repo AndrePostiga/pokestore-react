598dd045a2af4d29f2d8a675779f048886617fad
Andre Postiga <andre.postiga@msn.com>
2020-05-19 22:29:28 -0300

    Updates

---
+++ precache-manifest.bdc9c2ec4ab11a386f2b004cb0ff5755.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0589e51d0f4e97164f743a53dea5167e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cbb697504fe138e8a5da",
+    "url": "./static/js/2.cb41ff18.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.cb41ff18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a8ad4bced211bf2e5a0",
+    "url": "./static/js/main.385ffaec.chunk.js"
+  },
+  {
+    "revision": "245f2391f526e31157f8",
+    "url": "./static/js/runtime-main.450a138f.js"
+  }
+]);
